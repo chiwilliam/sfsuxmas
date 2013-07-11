e7d564b079c270c55c2a456b9a577bc88468dcac
dasarnow@gmail.com <dasarnow@gmail.com@2f5a60b6-3df1-11de-8fef-1f5ae417a2df>
2013-07-11 02:07:36 +0000

    Logic fixes for three traj files (clustered, collapsed, preserved)

--- trunk/src/com/sfsu/xmas/trajectory_files/SMakeTrajectoryFile.java
+++ trunk/src/com/sfsu/xmas/trajectory_files/SMakeTrajectoryFile.java
@@ -2,10 +2,12 @@ package com.sfsu.xmas.trajectory_files;
 
 import com.sfsu.xmas.session.SessionAttributeManager;
 import com.sfsu.xmas.util.StringUtils;
-import java.io.*;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class SMakeTrajectoryFile extends HttpServlet {
 
@@ -21,8 +23,9 @@ public class SMakeTrajectoryFile extends HttpServlet {
 
         if (fileName.length() > 0) {
             double binUnit = Double.parseDouble(request.getParameter("bin_unit"));
+            int k = Integer.parseInt(request.getParameter("kmeansk"));
 
-            TrajectoryFileGenerator gX = new TrajectoryFileGenerator(SessionAttributeManager.getActivePrimaryExpressionDatabase(request), binUnit);
+            TrajectoryFileGenerator gX = new TrajectoryFileGenerator(SessionAttributeManager.getActivePrimaryExpressionDatabase(request), binUnit, k);
 
             gX.generateFiles(fileName);
 
